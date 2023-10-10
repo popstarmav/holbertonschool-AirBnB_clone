f7b50ba110bc93d9e50d17f8b83260e007a41915
Charles Johnson <cj.johnson2017@gmail.com>
2023-10-09 21:09:53 -0700

    Define Place class

--- models/place.py
+++ models/place.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
+"""Defines the Place class."""
 from models.base_model import BaseModel
 
-
 class Place(BaseModel):
-    """Place class to represent rental places."""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.city_id = ""
-        self.user_id = ""
-        self.name = ""
-        self.description = ""
-        self.number_rooms = 0
-        self.number_bathrooms = 0
-        self.max_guest = 0
-        self.price_by_night = 0
-        self.latitude = 0.0
-        self.longitude = 0.0
-        self.amenity_ids = []
-
-    def __str__(self):
-        return "[{}] ({}) {} - {}".format(
-                self.__class__.__name__, self.id, self.name, self.city_id
-        )
+    """Represents a place with various attributes."""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
