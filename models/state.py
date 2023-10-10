6c874a26f186fbda0de57fa7f623a60d4306bb2e
Charles Johnson <cj.johnson2017@gmail.com>
2023-10-09 21:12:42 -0700

    Implement State class

--- models/state.py
+++ models/state.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
+"""Defines the State class."""
 from models.base_model import BaseModel
 
-
 class State(BaseModel):
-    """State class to represent states or regions."""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = ""
-
-    def __str__(self):
-        return "[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, self.name
-        )
+    """Represents a state with a 'name' attribute."""
+    name = ""
