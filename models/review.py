4f780353dbf2f45aa1faa1ff12e33765fe36f744
Charles Johnson <cj.johnson2017@gmail.com>
2023-10-09 21:11:08 -0700

    Add Review class

--- models/review.py
+++ models/review.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Review class to represent user reviews for places."""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
-
-    def __str__(self):
-        return "[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, self.text
-        )
+    """Represents a review with place_id,
+    user_id, and text attributes."""
+    place_id = ""
+    user_id = ""
+    text = ""
