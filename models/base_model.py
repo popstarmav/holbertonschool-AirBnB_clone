5863d4fbf2041cc1c54231dfee43fcc3656ada78
Charles Johnson <cj.johnson2017@gmail.com>
2023-10-10 17:08:17 -0700

    Implement BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,33 @@
 #!/usr/bin/python3
 """Defines BaseModel class."""
-import models
-from uuid import uuid4
-from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """Represents HBnB project BaseModel."""
-
     def __init__(self, *args, **kwargs):
-        """Initialize BaseModel with attributes from kwargs."""
-        date_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, date_format)
-                else:
-                    self.__dict__[k] = v
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(
+                            self, key, datetime.strptime(
+                                value, '%Y-%m-%dT%H:%M:%S.%f')
+                            )
+                    else:
+                        setattr(self, key, value)
         else:
-            models.storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        storage.new(self)
 
     def save(self):
-        """Update updated_at and save to storage."""
-        self.updated_at = datetime.today()
-        models.storage.save()
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """Return dictionary representation with class name."""
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
-
-    def __str__(self):
-        """Return string representation."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
