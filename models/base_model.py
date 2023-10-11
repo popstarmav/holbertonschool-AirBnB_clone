1ea18f0fd772a7510c0fb28ebc439db0db2f3dcd
Charles Johnson <cj.johnson2017@gmail.com>
2023-10-10 19:04:14 -0700

    Implement BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
-"""Defines BaseModel class."""
-from models import storage
-
+"""Base Model Module."""
+import uuid
+from datetime import datetime
+import models
 
 class BaseModel:
+    """Defines the BaseModel class."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance."""
         if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(
-                            self, key, datetime.strptime(
-                                value, '%Y-%m-%dT%H:%M:%S.%f')
-                            )
-                    else:
-                        setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-        storage.new(self)
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
+        """Update the `updated_at` attribute and save the instance to the storage engine."""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
+        """Return a dictionary containing all attributes of the instance."""
+        class_name = self.__class__.__name__
+        obj_dict = dict(self.__dict__)
+        obj_dict['__class__'] = class_name
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
